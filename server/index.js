802a1d76e5ddf89c2adaaaad5f0bafe894ed6c50
trishaMagical <trisha.hazra@madgicaltechdom.com>
2022-10-07 23:37:59 +0530

    Local Changes

--- server/index.js
+++ server/index.js
@@ -1,39 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const port = 3000;
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
-const port =3004;
-const server = http.createServer(app);
+const myapp = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:19006",
-    methods: ["GET", "POST"],
-  },
+app.get('/', (req, res) => res.send('Hello World!!!!'));
+const io = new Server(myapp, {
+    cors: {
+        origin: "http://localhost:19006",
+        methods: ["GET", "POST"],
+    },
 });
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
-    socket.on("join_room", (data)=>{
+    socket.on("join_room", (data) => {
         console.log(`User With ID:${socket.id} joined room: ${data}`);
         socket.join(data)
     });
-
-    socket.on("send_tictactoe",(data)=>{
-        console.log("send_tictactoe:",data);
+    socket.on("send_tictactoe", (data) => {
+        console.log("send_tictactoe:", data);
         socket.to(data.room).emit("recived_tictoc", data)
 
     });
-
-
     socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-      });
+        console.log("User Disconnected", socket.id);
+    });
 })
 
-
-server.listen(port, () => {
-  console.log("SERVER RUNNING:" + port);
-});
+myapp.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
