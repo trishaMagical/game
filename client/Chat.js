df9700411beb57f71d719c5b7d0adc1cd8f125d6
trishaMagical <trisha.hazra@madgicaltechdom.com>
2022-10-07 23:35:39 +0530

    Local Changes

--- client/Chat.js
+++ client/Chat.js
@@ -1,32 +1,47 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet, TouchableWithoutFeedback, Button, TextInput, TouchableOpacity, TouchEvent } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import App from "./App";
 
 export default function Chat({ socket, username, room }) {
-    const [click, setClick] = useState(1)
+    const [click, setClick] = useState(1);
     const [showChat, setShowChat] = useState(false);
     const [joinroom, setJoinRoom] = useState(false);
     const [boxes, setBoxes] = useState(
-       [{"value": ""}, {"value": ""}, {"value": ""},
-        {"value": ""}, {"value": ""}, {"value": ""},
-        {"value": ""},{"value": "" }, {"value": ""}
-       ]);
+        [{ "value": "" }, { "value": "" }, { "value": "" },
+        { "value": "" }, { "value": "" }, { "value": "" },
+        { "value": "" }, { "value": "" }, { "value": "" }
+        ]);
+    const [values, setValues] = useState(true);
+    const [newNames, setnewNames] = useState("");
+    const [recentvalue, setRecentvalue] = useState("");
     const handleClick = ((index) => {
         const boxValue = [...boxes]
-        if (click % 2 === 0 && boxValue[index].value === "" ) {
-            boxValue[index].value = "O";
-            setClick(click + 1);
-            console.log("Click", click);
+        if (click % 2 === 0 && boxValue[index].value === "") {
+            if (recentvalue === "" || recentvalue === "O") {
+                boxValue[index].value = "O";
+                setRecentvalue("O");
+                setClick(click + 1);
+            }
+            else {
+                alert("Other's Turn")
+            }
         }
-        else if(click % 2 != 0 && boxValue[index].value === "") {
-            boxValue[index].value = "X";
-            setClick(click + 1);
+        else if (click % 2 != 0 && boxValue[index].value === "") {
+            if (recentvalue === "" || recentvalue === "X") {
+                boxValue[index].value = "X";
+                setRecentvalue("X")
+                setClick(click + 1);
+            }
+            else {
+                alert("Other's Turn")
+            }
         }
         else {
             alert("Please Select A Empty Box!");
         }
     })
-    const winCalculation =(()=>{
+
+    const winCalculation = (() => {
         for (let i = 0; i < boxes.length; i++) {
             if (boxes[0].value === "X" && boxes[1].value === "X" && boxes[2].value === "X") {
                 alert("X winner & O Loser ")
@@ -109,82 +124,35 @@ export default function Chat({ socket, username, room }) {
                 setShowChat(true);
                 break;
             }
-             
-            else if(boxes[0].value != "" && boxes[1].value != "" && boxes[2].value != "" && 
-                    boxes[3].value != "" && boxes[4].value != "" && boxes[5].value != "" &&
-                    boxes[6].value != "" && boxes[7].value != "" && boxes[8].value != "" ){
+
+            else if (boxes[0].value != "" && boxes[1].value != "" && boxes[2].value != "" &&
+                boxes[3].value != "" && boxes[4].value != "" && boxes[5].value != "" &&
+                boxes[6].value != "" && boxes[7].value != "" && boxes[8].value != "") {
                 alert("Its a draw");
                 setShowChat(true);
                 break;
             }
-        }    
+        }
     })
-    const  chances =(()=>{
-         if((click % 2 != 0)&& (boxes[0].value != "O" && boxes[1].value != "O" && boxes[2].value != "O")){
-            alert("X should Move");
-        }
-        else if((click % 2 != 0)&& (boxes[3].value != "O" && boxes[4].value != "O" && boxes[5].value != "O")){
-            alert("X should Move"); 
-        }
-        else if((click % 2 != 0)&& (boxes[6].value != "O" && boxes[7].value != "O" && boxes[8].value != "O")){
-            alert("X should Move"); 
-        }
-        	
-        else if((click % 2 != 0)&& (boxes[0].value != "O" && boxes[3].value != "O" && boxes[6].value != "O")){
-            alert("X should Move"); 
-        }
-        else if((click % 2 != 0)&& (boxes[1].value != "O" && boxes[4].value != "0" && boxes[7].value != "O")){
-            alert("X should Move"); 
-        }
-        else if((click % 2 != 0)&& (boxes[2].value != "O" && boxes[5].value != "O" && boxes[8].value != "O")){
-            alert("X should Move"); 
-        }
-        else if((click % 2 != 0)&& (boxes[0].value != "O" && boxes[4].value != "O" && boxes[8].value != "O")){
-            alert("X should Move"); 
-        }
-        else if((click % 2 != 0) && (boxes[2].value != "O" && boxes[4].value != "O" && boxes[6].value != "O")){
-            alert("X should Move"); 
-        }
-        else if((click % 2 === 0) &&(boxes[0].value != "X" && boxes[1].value != "X" && boxes[2].value != "X")){
-            alert("O should Move");
-        }
-        else if((click % 2 === 0)&& (boxes[3].value != "X" && boxes[4].value != "X" && boxes[5].value != "X")){
-            alert("O should Move"); 
-        }
-          
-        else if((click % 2 === 0)&& (boxes[6].value != "X" && boxes[7].value != "X" && boxes[8].value != "X")){
-            alert("O should Move"); 
-        }
-        else if((click % 2 === 0)&& (boxes[0].value != "X" && boxes[3].value != "X" && boxes[6].value != "X")){
-            alert("O should Move"); 
-        }
-        else if((click % 2 === 0)&& (boxes[1].value != "X" && boxes[4].value != "X" && boxes[7].value != "X")){
-            alert("O should Move"); 
-        }
-        else if((click % 2 === 0)&& (boxes[2].value != "X" && boxes[5].value != "X" && boxes[8].value != "X")){
-            alert("O should Move"); 
-        }
-        else if((click % 2 === 0)&& (boxes[0].value != "X" && boxes[4].value != "X" && boxes[8].value != "X")){
-            alert("O should Move"); 
-        }
-        else if((click % 2 === 0)&& (boxes[2].value != "X" && boxes[4].value != "X" && boxes[6].value != "X")){
-            alert("O should Move"); 
-        }
-    
-    })  
+
+
     useEffect(() => {
-        //chances();
+
         socket.on("recived_tictoc", (data) => {
             console.log("Data", data);
             const dataValue = data.tic_tac_list
             console.log("setbocx", dataValue);
             setBoxes(dataValue);
             setClick(data.click_Number);
-            winCalculation()
+            const names = data.author
+            console.log("Name", names);
+            setnewNames(names);
         })
-    },[socket]);
+        winCalculation();
+    }, [socket]);
+    console.log("Hii", newNames);
     useEffect(() => {
-      chances();
+
         console.log("Click", click);
         const tictacdata = {
             room: room,
@@ -196,102 +164,230 @@ export default function Chat({ socket, username, room }) {
             click_Number: click,
         };
         socket.emit("send_tictactoe", tictacdata);
-        winCalculation()
+        winCalculation();
     }, [click])
+
     const joinRoom = () => {
-       setJoinRoom(true);
-      }
+        setJoinRoom(true);
+    }
+    const exitRoom = () => {
+        if (click != 0) {
+            alert("You Will Lose the game!!!")
+            setBoxes([{ "value": "" }, { "value": "" }, { "value": "" },
+            { "value": "" }, { "value": "" }, { "value": "" },
+            { "value": "" }, { "value": "" }, { "value": "" }
+            ]);
+            setShowChat(true);
+        }
+
+    }
+    const joinRoomAgain = () => {
+
+        if (username !== "" && room !== "") {
+            socket.emit("join_room", room);
+            setBoxes([{ "value": "" }, { "value": "" }, { "value": "" },
+            { "value": "" }, { "value": "" }, { "value": "" },
+            { "value": "" }, { "value": "" }, { "value": "" }
+            ]);
+            setShowChat(false)
+
+        }
+    }
     return (
         <View>
-         {!showChat ? (
-            	
-      
-      <View style={styles.mainContainer}>
-      <View style={styles.container}>
-          <Text style={styles.useNameText}>{username}</Text>
-          <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-              {
-                  boxes.map((s1, index) => <View style={{ flexBasis: "30%", borderRadius: 3, backgroundColor: "yellow", borderWidth: 3, textAlign: "center" }} key={index}>
-                      <TouchableOpacity style={{
-                          height: 30,
-                          fontSize: 40,
-                          textAlign: "center",
-                          marginLeft: 20,
-                          marginTop: 15,
-                      }}
-                          onPress={() => handleClick(index, s1)}
-                      >
-                          <Text>{s1.value}</Text>
-                      </TouchableOpacity>
-                  </View>)
-              }
-          </View>
-      </View>
-  </View>
-         ) 
-         :
-          (
-           <View style={styles.mainContainer}>
-            {
-              !joinroom ?  (
-                <View>
-                <View style={styles.container}>
-                <Text style={styles.useNameText}>Game End !!!</Text>
-                <TouchableOpacity
-                style={styles.button}
-                onPress={joinRoom}
-                >
-                <Text style={styles.buttonText}>Want to Play Again?</Text>
-                </TouchableOpacity>
-                </View>
-                </View>
-              ) 
-              :(
+            {!showChat ? (
                 <View style={styles.mainContainer}>
                     <View style={styles.container}>
-                         <App/>
+                        <View style={styles.headingContainer}>
+                            <Text style={click % 2 != 0 ? { color: "red", fontSize: 25, } : { color: "grey", fontSize: 25, }}>
+                                Player's Name: {username}
+                            </Text>
+                            <Text style={click % 2 === 0 ? { color: "red", fontSize: 25, } : { color: "grey", fontSize: 25, }}>
+                                Other Player Name: {newNames}
+                            </Text>
+                        </View>
+
+                        <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "center" }}>
+                            {
+                                boxes.map((s1, index) => <View style={{
+                                    borderRadius: 3,
+                                    backgroundColor: "yellow",
+                                    borderWidth: 3,
+
+                                }} key={index} >
+                                    <TouchableOpacity style={{
+                                        height: 100,
+                                        width: 100,
+                                        fontSize: 40,
+
+                                    }}
+                                        onPress={() => handleClick(index, s1)}
+                                    >
+                                        <Text style={{
+                                            color: "red", marginRight: 8,
+                                            fontSize: 70, textAlign: "center"
+                                        }}>{s1.value}</Text>
+                                    </TouchableOpacity>
+                                </View>)
+                            }
+
+                        </View>
+                        <View style={styles.exitRoommainContainer}>
+                            <TouchableOpacity
+                                style={styles.exitRoomstyle}
+                                onPress={exitRoom}
+                            >
+                                <Text style={styles.buttonTextforExitroom}>Want to Quit?</Text>
+                            </TouchableOpacity>
+
+                        </View>
+
                     </View>
-                </View>    
-              )
-            }
-           </View> 
-         
-            )}  
+                </View>
+            )
+                :
+                (
+                    <View>
+                        {
+                            !joinroom ? (
+                                <View >
+                                    <View style={styles.gameEndContainer}>
+                                        <Text style={styles.useNameText}>Game End !!!</Text>
+                                        <TouchableOpacity
+                                            style={styles.button}
+                                            onPress={joinRoom}
+                                        >
+                                            <Text style={styles.buttonText}>Want to Play Again?</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            )
+                                : (
+                                    <View>
+
+                                        <View>
+
+                                            <View style={styles.mainContainerforPlayagain}>
+                                                <View style={styles.containerforPlayagain}>
+                                                    <Text style={styles.headerText}>Join A Chat</Text>
+                                                    <TextInput
+                                                        style={styles.textInput}
+                                                        value={username}
+
+                                                    />
+                                                    <TextInput
+                                                        style={styles.textInput}
+                                                        value={room}
+
+                                                    />
+                                                    <TouchableOpacity
+                                                        style={styles.button}
+                                                        onPress={joinRoomAgain}
+                                                    >
+                                                        <Text style={styles.buttonText}>Join A Room</Text>
+                                                    </TouchableOpacity>
+                                                </View>
+                                            </View>
+
+
+
+                                        </View>
+
+
+
+                                    </View>
+
+                                )
+                        }
+                    </View>
+
+
+                )}
         </View>
-          
+
     )
 }
 const styles = StyleSheet.create({
     mainContainer: {
         marginTop: 100,
         marginBottom: 10,
-        marginRight: 120,
-        marginLeft: 120,
-        alignItems: 'center',
-        justifyContent: 'center'
+
     },
     container: {
         marginBottom: 50,
         backgroundColor: '#fff',
+
+    },
+    headingContainer: {
+        marginBottom: 5,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    textInput: {
+        backgroundColor: "yellow",
+        padding: 4,
+        borderColor: "black",
+        borderWidth: 4,
+        marginBottom: 15,
+        width: 300,
+        height: 60,
     },
     useNameText: {
         color: "red",
         fontSize: 25,
-    },
-    button:{
-        marginTop:20,
-        width:300,
-        height:40,
-        backgroundColor:"green",
-    
-      },
-      buttonText:{
-        justifyContent:"center",
-        textAlign:"center",
-        marginTop:10,
-        color:"white"
-      }
+        marginTop: 90,
 
+        textAlign: "center"
+    },
+    button: {
+        marginTop: 20,
+        width: 175,
+        height: 40,
+        backgroundColor: "green",
+        marginLeft: 120,
+
+    },
+    buttonText: {
+        justifyContent: "center",
+        textAlign: "center",
+        marginTop: 10,
+        color: "white"
+    },
+    exitRoommainContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    exitRoomstyle: {
+        marginTop: 20,
+        width: 190,
+        height: 60,
+        backgroundColor: "green",
+    },
+    buttonTextforExitroom: {
+        fontSize: 25,
+        justifyContent: "center",
+        textAlign: "center",
+        marginTop: 10,
+        color: "white"
+    },
+    mainContainerforPlayagain: {
+        marginTop: 200,
+        marginBottom: 10,
+        marginRight: 120,
+        marginLeft: 120,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containerforPlayagain: {
+        marginTop: 15,
+        marginBottom: 50,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 10,
+    }
 });
